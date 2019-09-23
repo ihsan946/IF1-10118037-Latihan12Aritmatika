5e9c9760b8dda5c5b227223746fe0f631e085819
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 15:14:15 +0700

    Add files via upload

---
+++ IF110118037Latihan12Aritmatika/src/if110118037latihan12aritmatika/IF110118037Latihan12Aritmatika.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan12aritmatika;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program Aritmatika.
+ */
+public class IF110118037Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " +(a + b));
+        System.out.println("a - b = " +(a - b));
+        System.out.println("a * b = " +(a * b));
+        System.out.println("b / a = " +(b / a));
+        System.out.println("b % a = " +(b % a));
+        System.out.println("c % a = " +(c % a));
+        System.out.println("a++ = " +(a++));
+        System.out.println("b-- = " +(b--));
+        System.out.println("d++ = " +(d++));
+        System.out.println("++d = " +(++d));
+    }
+    
+}
